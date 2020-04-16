e9250c96834faa8721d79337b74e22360a3ec8b3
Olivier Anthony <sebastiananthony0702@gmail.com>
2020-04-16 22:45:09 +0700

    Update PilihFile.c

--- BukaFile/PilihFile.c
+++ BukaFile/PilihFile.c
@@ -1,3 +1,14 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 9 - TUGAS BESAR
+* Kelompok : 2
+* Hari dan Tanggal : Kamis, 9 April 2020
+* Asisten (NIM) : Devi Tara Avalokita (18316024)
+* Nama File :PilihFile.c
+* Deskripsi : Pengambilan File Referensi
+*/
+
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
