66e50cd3a9a93daa3fe0ab3ab949074aebb86e4b
hamersda <49382117+hamersda@users.noreply.github.com>
2020-04-16 19:19:54 +0700

    Add files via upload

--- Table/Prototype/TableV1.c
+++ Table/Prototype/TableV1.c
@@ -1,8 +1,6 @@
-#include "stdio.h"
-#include "string.h"
-#include "stdlib.h"
-#include "Table.h"
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 //struct table n_gram
 struct n_gram{
     char key[50];
@@ -101,3 +99,86 @@ void table_ngram(char **w,int tingkat,int n,struct n_gram **head_ref){
         }
     }
 }
+/*
+//Contoh pemakaian
+int main(){
+    //inisiasi variable
+        char w[10][10],temp[30];
+        char enter;
+        int n,i,tingkat;
+        struct n_gram *head=NULL;
+    //Contoh parsing kata
+        strcpy(w[0],"kenapa");
+        strcpy(w[1],"sadis");
+        strcpy(w[2],"luar");
+        strcpy(w[3],"kenapa");
+        strcpy(w[4],"begini");
+        strcpy(w[5],"aku");
+        strcpy(w[6],"berbaikanlah");
+        strcpy(w[7],"niscaya");
+        strcpy(w[8],"semesta");
+        strcpy(w[9],"mendukungmu");
+    printf("n-gram berapa: ");
+    scanf("%d",&tingkat);
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    scanf("%c",&enter);
+    //Membuat Circular linked list dari belakang
+    for (i = n; i >= 0; --i){
+        //n-gram 1
+        if (tingkat==1){
+            if (n!=i){
+                push(&head,w[i],w[i+1]);
+            }
+            else if (n==i){
+                push(&head,w[i],w[0]);
+            }
+        }
+        //n-gram 2
+        else if(tingkat==2){
+            if (i==n){
+                strcpy(temp,w[i]);
+                strcat(temp,w[0]);
+                push(&head,temp,w[1]);
+            }
+            else if (i==n-1){
+                strcpy(temp,w[i]);
+                strcat(temp,w[i+1]);
+                push(&head,temp,w[0]);
+            }
+            else{
+                strcpy(temp,w[i]);
+                strcat(temp,w[i+1]);
+                push(&head,temp,w[i+2]);
+            }
+        }
+        //n-gram 3
+        else if(tingkat==3){
+            if (i==n){
+                strcpy(temp,w[i]);
+                strcat(temp,w[0]);
+                strcat(temp,w[1]);
+                push(&head,temp,w[2]);
+            }
+            else if (i==n-1){
+                strcpy(temp,w[i]);
+                strcat(temp,w[n]);
+                strcat(temp,w[0]);
+                push(&head,temp,w[1]);
+            }
+            else if (i==n-2){
+                strcpy(temp,w[i]);
+                strcat(temp,w[n-1]);
+                strcat(temp,w[n]);
+                push(&head,temp,w[0]);
+            }
+            else{
+                strcpy(temp,w[i]);
+                strcat(temp,w[i+1]);
+                strcat(temp,w[i+2]);
+                push(&head,temp,w[i+3]);
+            }
+        }
+    }
+    return 0;
+}*/
