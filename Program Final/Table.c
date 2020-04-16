a73c942491f7c364e8aa91f441a28c67b40136a2
hamersda <49382117+hamersda@users.noreply.github.com>
2020-04-16 22:53:40 +0700

    Update Table.c

--- Program Final/Table.c
+++ Program Final/Table.c
@@ -1,3 +1,12 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 9 - TUGAS BESAR
+* Kelompok : 2
+* Hari dan Tanggal : Jumat, 10 April 2020
+* Asisten (NIM) : Devi Tara Avalokita (18316024)
+* Nama File : Table.c
+* Deskripsi : Program untuk membuat table n-gram
+*/
+
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
