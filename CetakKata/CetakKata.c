6c894a4994ea421cf81a41e248165813412c6f74
Olivier Anthony <sebastiananthony0702@gmail.com>
2020-04-17 18:32:06 +0700

    Update CetakKata.c

--- CetakKata/CetakKata.c
+++ CetakKata/CetakKata.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "CetakKata.h"
+struct n_gram{
+    char key[50];
+    char value[10];
+    struct n_gram *next;
+};
 
-char CetakKata(int nkata, struct n_gram *ambiltabel)
+/*char CetakKata(int nkata, struct n_gram *ambiltabel)
 {
     int kataawalacak, k, i, j, pilihrandomvalue, tingkat;
     char kata_dicetak;
@@ -31,4 +36,24 @@ char CetakKata(int nkata, struct n_gram *ambiltabel)
         i=i+1;
     }
     printf("...");
+}*/
+void CetakKata(int nkata, struct n_gram *ambiltabel){
+    int r_numb, i,j;
+    struct n_gram* temp = (struct n_gram*) malloc(sizeof(struct n_gram));
+    temp = ambiltabel->next;
+    r_numb = rand()%100;
+    printf("...");
+    for(i=0;i<r_numb;i++){
+        ambiltabel = temp;
+        temp = ambiltabel->next;
+    }
+    for(j=0;j<nkata;j++){
+        printf("%s ", ambiltabel->value);
+        temp = ambiltabel->next;
+        ambiltabel = temp;
+    }
+printf("...");
+
 }
+
+
