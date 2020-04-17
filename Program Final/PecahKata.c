9ef280fd537d59bb328261f2677d83cd9f28e0a1
MatthewTerrence <60211021+MatthewTerrence@users.noreply.github.com>
2020-04-17 14:01:30 +0700

    Update PecahKata.c

--- Program Final/PecahKata.c
+++ Program Final/PecahKata.c
@@ -4,13 +4,16 @@
 
 char PecahKata( char *kata)
 {
+    char kalimat[100];
+    char *kata = kalimat;
     char array_words[10][10]; 
     int i,j,cnt;
     j=0; 
     cnt=0;
+    
     for(i=0;i<=(strlen(kalimat));i++)
     {
-        if( *kata==' '|| *kata =='\0')  // ditemukan spasi atau Null, asign array_words[cnt]
+        if( kalimat[i]==' '|| kalimat[i] =='\0')  // ditemukan spasi atau Null, asign array_words[cnt]
         {
             array_words[cnt][j]='\0';
             cnt++;  
