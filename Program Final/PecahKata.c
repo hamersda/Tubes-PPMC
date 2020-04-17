f1a17ae53908a15b159691ca6e3fb67978a6989e
MatthewTerrence <60211021+MatthewTerrence@users.noreply.github.com>
2020-04-17 13:39:01 +0700

    Add files via upload

---
+++ Program Final/PecahKata.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char * PecahKata( char *kata)
+{
+    char *array_words[10][10]; 
+    int i,j,cnt;
+    j=0; 
+    cnt=0;
+    for(i=0;i<=(strlen(kalimat));i++)
+    {
+        if( *kata==' '|| *kata =='\0')  // ditemukan spasi atau Null, asign array_words[cnt]
+        {
+            array_words[cnt][j]='\0';
+            cnt++;  
+            j=0;    
+        }
+        else
+        {
+            array_words[cnt][j]=kalimat[i];
+            j++;
+        }
+    }
+    return array_words;
+}
+
+
